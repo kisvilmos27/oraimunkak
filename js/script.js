a392b287d03cb245553a0d37cbf68b578e6d51a2
kisvilmos27 <kisbendeguz2007@gmail.com>
2023-03-17 12:19:10 +0100

    js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.writeln("ayaya<br>");
+document.writeln("ayayaya<br>");
+document.writeln("ayayayaya<br>");
+document.write("A");
+document.write("B");
+document.write("D<br>");
+zam = 69;
+document.writeln("lö zam: ",zam,"<br>");
+document.writeln("lö ne zam: ",zam*-1,"<br>");
+document.writeln("lö ha zam: ",zam/2,"<br>");
+document.writeln("lö sq zam: ",zam**2,"<br>");
+document.writeln("lö xo zam: ",zam^2,"<br>");
+for(var i=1;i<11;i++){
+    document.write("<h4>szam: ", i, "; negyzet: ", i**2,"</h4>");
+}
+document.write(12%7);
+document.write("<br><br><br><br><br>")
+/*
+asdasd
+asd
+*/
+for(var i = 0; i<12;i++){
+    document.write(i, " : ", i%2,"<br>");
+}
